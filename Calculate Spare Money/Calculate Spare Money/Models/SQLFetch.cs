00c3b9d9a6535c421d19fc95abbfd08e522bf037
Tony Henderson <th_rams94@hotmail.com>
2020-11-02 09:41:28 -0600

    Added properties and methods to SQLFetch class

--- Calculate Spare Money/Calculate Spare Money/Models/SQLFetch.cs
+++ Calculate Spare Money/Calculate Spare Money/Models/SQLFetch.cs
@@ -5,12 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace Calculate_Spare_Money.Models
 {
     class SQLFetch
     {
 
-        private string _connectionString = "server=(local)\\SQLEXPRESS01;database=Bills;Integrated Security=SSPI;";
+        public const string connectionString = "server=(local)\\SQLEXPRESS01;database=Bills;Integrated Security=SSPI;";
+        private SqlCommand _sqlCommand;        
+
+        public SQLFetch()
+        {
+
+        }
+        public SQLFetch(string sqlCommand)
+        {
+            _sqlCommand = new SqlCommand(sqlCommand);
+        }
+
+        public void setSqlCommand(string sqlCommand)
+        {
+            _sqlCommand = new SqlCommand(sqlCommand); ;
+        }
+
+        public void setParameter(string paramReference, SqlDbType sqlDbType, string value)
+        {
+            _sqlCommand.Parameters.Add(paramReference, sqlDbType).Value = value;
+        }
     }
+
 }
